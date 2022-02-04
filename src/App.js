95f57d50e4d9dae07edbd26c3e99ed11237f70d6
brisibe <43288228+jbrisibe10@users.noreply.github.com>
2022-02-04 15:10:44 +0100

    change project structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AppLayout from "./containers/layout/AppLayout";
+import LoginPage from "./pages/auth/login/LoginPage";
+import PasswordReset from "./pages/auth/passwordReset/PasswordReset";
+import RegisterPage from "./pages/auth/register/RegisterPage";
+import HomePage from "./pages/home/HomePage";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <h1>FabGarage</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="passwordreset" element={<PasswordReset />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+      <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<RegisterPage />} />
+    </Routes>
   );
 }
 
---
+++ src/containers/layout/AppLayout.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar/Navbar";
+
+const AppLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default AppLayout;
