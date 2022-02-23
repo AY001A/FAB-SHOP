d0a9962012a210a9c2459a0d2c824b730f6edfe1
brisibe <43288228+jbrisibe10@users.noreply.github.com>
2022-02-23 09:41:34 +0100

    route based code splitting

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
+import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "./containers/layout/AppLayout/AppLayout";
-import AuthLayout from "./containers/layout/AuthLayout/AuthLayout";
-import ForgetPassword from "./pages/auth/forgetPassword/ForgetPassword";
-import LoginPage from "./pages/auth/login/LoginPage";
-import RegisterPage from "./pages/auth/register/RegisterPage";
-import HomePage from "./pages/home/HomePage";
-import ForgetPasswordSuccess from "./pages/auth/forgetPassword/ForgetPasswordSuccess";
-import NotFound from "./pages/notFound/NotFound";
-import PasswordReset from "./pages/auth/forgetPassword/PasswordReset";
+
+const HomePage = lazy(() => import("./pages/home/HomePage"));
+const LoginPage = lazy(() => import("./pages/auth/login/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/auth/register/RegisterPage"));
+const NotFound = lazy(() => import("./pages/notFound/NotFound"));
+const PasswordReset = lazy(() =>
+  import("./pages/auth/passwordReset/PasswordReset")
+);
+const AuthLayout = lazy(() =>
+  import("./containers/layout/AuthLayout/AuthLayout")
+);
+const ForgetPassword = lazy(() =>
+  import("./pages/auth/forgetPassword/ForgetPassword")
+);
+const ForgetPasswordSuccess = lazy(() =>
+  import("./pages/auth/forgetPassword/ForgetPasswordSuccess")
+);
 
 function App() {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="forget-password" element={<ForgetPassword />} />
-        <Route
-          path="forget-password/success"
-          element={<ForgetPasswordSuccess />}
-        />
-        <Route path="resetpassword/" element={<PasswordReset />}>
-          {/* <Route path=":ownerId" element={<PasswordReset />} /> */}
-        </Route>
+    <Suspense fallback={<div>loading</div>}>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="forget-password" element={<ForgetPassword />} />
+          <Route
+            path="forget-password/success"
+            element={<ForgetPasswordSuccess />}
+          />
+          <Route path="resetpassword/" element={<PasswordReset />}>
+            {/* <Route path=":ownerId" element={<PasswordReset />} /> */}
+          </Route>
 
-        <Route path="*" element={<NotFound />} />
-      </Route>
-      <Route element={<AuthLayout />}>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
-    </Routes>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
