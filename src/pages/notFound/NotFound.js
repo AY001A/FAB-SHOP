3f1ee976fb1d349aa36a26a1ac7a8d74dd4fb845
AY001A <ayoola91@gmail.com>
2022-03-01 21:27:14 +0530

    issue #4

--- src/pages/notFound/NotFound.js
+++ src/pages/notFound/NotFound.js
@@ -6,14 +6,13 @@ const NotFound = () => {
 
 
   
-  return <center> 
-    <div class="p-5 m-5 ">
-      <p class="fs-1 fw-bold">404</p>
-      <p class="fs-2 fw-normal">page not found</p>
-      <h5 class="p-3 m-3">This page you are looking for does not exit or was removed.<br/>we have alot of interesting products for you on the homepage</h5>
-       <Link to="/"> <button class="btn btn-primary d-grid gap-2 col-6 fw-bold" type="button">Return to Homepage</button></Link>
+  return  <div className="notFound p-5 m-5 ">
+      <p className="fs-1 fw-bold">404</p>
+      <p className="fs-2 fw-normal">page not found</p>
+      <h5 className="p-3 m-3">This page you are looking for does not exit or was removed.<br/>we have alot of interesting products for you on the homepage</h5>
+       <Link to="/"> <button className="btn btn-primary d-grid gap-2 col-6 fw-bold" type="button">Return to Homepage</button></Link>
       </div>
-    </center>
+  
 };
 
 export default NotFound;
