545aaff36bb943f9c28f934b37af3dc7fcf3e454
AY001A <ayoola91@gmail.com>
2022-03-03 00:22:58 +0530

    not found queries

--- src/pages/notFound/NotFound.js
+++ src/pages/notFound/NotFound.js
@@ -7,9 +7,9 @@ const NotFound = () => {
 
   
   return  <div className="notFound p-5 m-5 ">
-      <p className="fs-1 fw-bold">404</p>
-      <p className="fs-2 fw-normal">page not found</p>
-      <h5 className="p-3 m-3">This page you are looking for does not exit or was removed.<br/>we have alot of interesting products for you on the homepage</h5>
+      <p className="fof fs-1 fw-bold">404</p>
+      <p className="page fs-2 fw-normal">page not found</p>
+      <h5 className="txt p-3 m-3">This page you are looking for does not exit or was removed.<br/>we have alot of interesting products for you on the homepage</h5>
        <Link to="/"> <button className="btn btn-primary d-grid gap-2 col-6 fw-bold" type="button">Return to Homepage</button></Link>
       </div>
   
