760d10262ebe63ddbdd58af4a51124ccfc9d0237
brisibe <43288228+jbrisibe10@users.noreply.github.com>
2022-03-07 15:11:28 +0100

    refactor layout

--- src/containers/layout/AuthLayout/AuthLayout.js
+++ src/containers/layout/AuthLayout/AuthLayout.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { Children } from "react";
 import { Link, Outlet } from "react-router-dom";
 import Logo from "../../../assets/icons/cucumislogo.svg";
 import "./style.scss";
 
-const AuthLayout = () => {
+const AuthLayout = ({ Children }) => {
   return (
     <div className="outer-wrapper">
       <div className="content-wrapper">
@@ -12,9 +12,7 @@ const AuthLayout = () => {
             <img src={Logo} className="nav-logo logo" alt="brand logo" />
           </Link>
         </div>
-        <div className="form-container">
-          <Outlet />
-        </div>
+        <div className="form-container">{Children}</div>
       </div>
       <div className="image-wrapper"></div>
     </div>
