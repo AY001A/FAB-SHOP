17730fc54bf61429dd20557e6272fe29e698cf80
brisibe <43288228+jbrisibe10@users.noreply.github.com>
2022-04-04 22:36:58 +0100

    cart component

---
+++ src/components/cards/cart-card/CartCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CartCard = () => {
+  return (
+    <div className="cart-item-wrapper">
+      <div className="cart-item-container">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default CartCard;
