a4afbb91b310f64e11dc2a5d6e21dcbab3227591
Kingsley Onyeka Ifeanyi <80105176+Techillionaire@users.noreply.github.com>
2022-04-06 14:24:37 +0100

    Cart card done

--- src/components/cards/cart-card/CartCard.js
+++ src/components/cards/cart-card/CartCard.js
@@ -15,13 +15,13 @@ const CartCard = () => {
 
             <div>
               <div className="cart-title-wrapper">
-                <p>Gold Metal Iron Fabr</p>
-                <p>Product Code: 939393</p>
+                <p className="fw-bold fs-5">Gold Metal Iron Fabricated gate</p>
+                <p className='fw-normal text-muted'>Product Code: 939393</p>
               </div>
 
               <div className="cart-delete-btn">
                 {/* <img src={DeleteIcon} /> */}
-                <DeleteIcon width={20} height={20}/>
+                <DeleteIcon width={35} height={35}/>
               </div>
             </div>
           </div>
@@ -29,7 +29,43 @@ const CartCard = () => {
         </div>
 
         <div className="cart-item-details">
-          counter
+          <div className='row align-items-start quantityRow'>
+            <div className='col'>
+              <div className='quantityAdjustment row align-items-center fs-6 text-secondary'>Quantity : </div>
+            </div>
+
+            <div className='col quantityCol'>
+              <button className='decreaseButton'>-</button>
+              <p className='quantityFigure'>1</p>
+              <button className='increaseButton'>+</button>
+            </div>
+
+          </div>
+
+          
+          <div className="row align-items-center unitRow">
+            <div className="col">
+              <div className='unitPrice row align-items-center fs-6 text-secondary'>Unit : </div>
+
+            </div>
+            <div className="col">
+              <div className='text-dark unitPriceFigure'>₦70,000</div>
+            </div>
+          </div>
+
+          <div className="row align-items-end subtotalRow">
+            <div className="col">
+              <div className='subtotalPrice row align-items-center fs-6 text-secondary'>Sub total:</div>
+
+            </div>
+            <div className="col">
+              <div className='subtotalPriceFigure text-dark'>₦70,000</div>
+            </div>
+          </div>
+
+          
+          
+          
         </div>
       </div>
     </div>
