e366d60fccaea4a7cd45964dc9c132775aefa13a
joseph <jbrisibe@gmail.com>
2022-04-06 11:44:58 +0100

    fix build error

--- src/pages/cart/CartPage.js
+++ src/pages/cart/CartPage.js
@@ -7,24 +7,24 @@ const CartPage = () => {
   const { cartQuantity } = useSelector((state) => state.cart);
 
   return (
-    // <div className="cart-page-wrapper">
-    //   <div className="cart-container-wrapper">
-    //     <div className="cart-card-wrapper">
-    //       <div className="cart-header">
-    //         <h2>Cart </h2> <p>({cartQuantity} Items)</p>
-    //       </div>
-    //       <CartCard />
-    //     </div>
-    //     <div className="cart-price-card">holla</div>
-    //   </div>
-    // </div>
-    <div className="row">
-      <div className=" col-8 ">main menul</div>
-      <div className="col-4  ">
-        {" "}
-        <div>side bar</div>
+    <div className="cart-page-wrapper">
+      <div className="cart-container-wrapper">
+        <div className="cart-card-wrapper">
+          <div className="cart-header">
+            <h2>Cart </h2> <p>({cartQuantity} Items)</p>
+          </div>
+          <CartCard />
+        </div>
+        <div className="cart-price-card">holla</div>
       </div>
     </div>
+    // <div className="row">
+    //   <div className=" col-8 ">main menul</div>
+    //   <div className="col-4  ">
+    //     {" "}
+    //     <div>side bar</div>
+    //   </div>
+    // </div>
   );
 };
 
