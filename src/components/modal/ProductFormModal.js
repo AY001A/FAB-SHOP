ea8f4a744c3fa1164d13117089c73b45384857fa
Emmanuel <84718297+iamemmanuel-dev@users.noreply.github.com>
2022-07-12 19:39:07 +0100

    bug-fix (#60)

--- src/components/modal/ProductFormModal.js
+++ src/components/modal/ProductFormModal.js
@@ -1,131 +1,160 @@
-import React, { useState } from "react";
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
-import { useFormik } from "formik";
-import "./style.scss";
+import React, { useState } from 'react'
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+import { useFormik } from 'formik'
+import './style.scss'
+import { useCreateServiceBooking } from '../../hook/useService'
+import { toast } from 'react-toastify'
 
-const ProductFormModal = (props) => {
+const ProductFormModal = props => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [modalShow, setModalShow] = useState(false)
+
+  const mutation = useCreateServiceBooking()
+
   const formik = useFormik({
     initialValues: {
-      firstname: "",
-      lastname: "",
-      email: "",
-      phone: "",
-      address: "",
-      description: "",
+      firstname: '',
+      lastname: '',
+      email: '',
+      phone: '',
+      address: '',
+      description: '',
     },
 
     // validationSchema: serviceSchema,
 
-    onSubmit: async (values, { setSubmitting }) => {
-      // dispatch(login(values));
-      console.log(values);
+    onSubmit: async values => {
+      setIsLoading(true)
+      mutation.mutate(
+        {
+          fullname: `${values.firstname} ${values.lastname}`,
+          emailAddress: values.email,
+          phoneNumber: values.phone,
+          // categoryId: currentService?.Category?.Id,
+          // serviceId: currentService?.Id,
+        },
+        {
+          onSuccess() {
+            setIsLoading(false)
+            toast.success('awesome!')
+          },
+          onError() {
+            toast.warn('oops! something went wrong, try again later')
+            setIsLoading(false)
+          },
+        }
+      )
     },
-  });
+  })
 
   return (
     <Modal
       {...props}
-      size="lg"
-      aria-labelledby="example-modal-sizes-title-lg"
-      backdrop="static"
+      size='lg'
+      aria-labelledby='example-modal-sizes-title-lg'
+      backdrop='static'
       centered
-      className="service-modal service-modal"
+      className='service-modal service-modal'
     >
       <Form
         noValidate
-        className="requires-validation"
+        className='requires-validation'
         onSubmit={formik.handleSubmit}
       >
         <Modal.Header closeButton>
           <Modal.Title>Place order</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div className="row g-3">
-            <div className="form-group col-6">
+          <div className='row g-3'>
+            <div className='form-group col-6'>
               <Form.Control
-                type="text"
-                className="form-control service-modal-input"
-                id="firstname"
-                name="firstname"
-                size="md"
-                placeholder="First name"
+                type='text'
+                className='form-control service-modal-input'
+                id='firstname'
+                name='firstname'
+                size='md'
+                placeholder='First name'
                 onChange={formik.handleChange}
                 value={formik.values.firstname}
                 isInvalid={formik.touched.firstname && formik.errors.firstname}
               />
-              <div className="invalid-feedback">{formik.errors.firstname}</div>
+              <div className='invalid-feedback'>{formik.errors.firstname}</div>
             </div>
-            <div className="form-group mb-3 col-6">
+            <div className='form-group mb-3 col-6'>
               <Form.Control
-                type="text"
-                className="form-control"
-                id="lastname"
-                size="md"
-                placeholder="Last name"
-                name="lastname"
+                type='text'
+                className='form-control'
+                id='lastname'
+                size='md'
+                placeholder='Last name'
+                name='lastname'
                 onChange={formik.handleChange}
                 value={formik.values.lastname}
                 isInvalid={formik.touched.lastname && formik.errors.lastname}
               />
-              <div className="invalid-feedback">{formik.errors.lastname}</div>
+              <div className='invalid-feedback'>{formik.errors.lastname}</div>
             </div>
           </div>
-          <div className="row g-3">
-            <div className="form-group col-6">
+          <div className='row g-3'>
+            <div className='form-group col-6'>
               <Form.Control
-                type="email"
-                className="form-control"
-                id="email"
-                size="md"
-                placeholder="Email Address"
-                name="email"
+                type='email'
+                className='form-control'
+                id='email'
+                size='md'
+                placeholder='Email Address'
+                name='email'
                 onChange={formik.handleChange}
                 value={formik.values.email}
                 isInvalid={formik.touched.email && formik.errors.email}
               />
-              <div className="invalid-feedback">{formik.errors.email}</div>
+              <div className='invalid-feedback'>{formik.errors.email}</div>
             </div>
 
-            <div className="form-group  col-6">
+            <div className='form-group  col-6'>
               <Form.Control
-                type="text"
-                className="form-control"
-                id="phone"
-                name="phone"
-                size="md"
-                placeholder="Phone Number"
+                type='text'
+                className='form-control'
+                id='phone'
+                name='phone'
+                size='md'
+                placeholder='Phone Number'
                 onChange={formik.handleChange}
                 value={formik.values.phone}
                 isInvalid={formik.touched.phone && formik.errors.phone}
               />
-              <div className="invalid-feedback">{formik.errors.phone}</div>
+              <div className='invalid-feedback'>{formik.errors.phone}</div>
             </div>
-            <div className="form-group ">
+            <div className='form-group '>
               <Form.Control
-                type="text"
-                className="form-control"
-                id="address"
-                size="md"
-                name="address"
-                placeholder="Address"
+                type='text'
+                className='form-control'
+                id='address'
+                size='md'
+                name='address'
+                placeholder='Address'
                 onChange={formik.handleChange}
                 value={formik.values.address}
                 isInvalid={formik.touched.address && formik.errors.address}
               />
-              <div className="invalid-feedback">{formik.errors.address}</div>
+              <div className='invalid-feedback'>{formik.errors.address}</div>
             </div>
           </div>
         </Modal.Body>
 
         <Modal.Footer>
-          <button type="submit" className="btn btn-primary p-2">
-            Request Service Quote
+          <button type='submit' className='btn btn-primary p-2'>
+            {isLoading ? (
+              <strong>Submitting...</strong>
+            ) : (
+              <strong>Request Service Quote</strong>
+            )}
           </button>
         </Modal.Footer>
       </Form>
     </Modal>
-  );
-};
+  )
+}
 
-export default ProductFormModal;
+export default ProductFormModal
--- src/hook/useService.js
+++ src/hook/useService.js
@@ -1,17 +1,17 @@
-import { useMutation, useQuery } from "react-query";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import { bookService, listServices } from "../api/services";
+import { useMutation, useQuery } from 'react-query'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { bookService, listServices } from '../api/services'
 
 export function useCreateServiceBooking() {
-  return useMutation((service) => bookService(service), {
+  return useMutation(service => bookService(service), {
     onSuccess: () => {},
-    onError: (error) => {
-      toast.error("oops! please try again later: ", error);
+    onError: error => {
+      toast.error('oops! please try again later: ', error)
     },
-  });
+  })
 }
 
 export function useGetServiceList(currentPage, pageSize) {
-  return useQuery("services", () => listServices(currentPage, pageSize));
+  return useQuery('services', () => listServices(currentPage, pageSize))
 }
