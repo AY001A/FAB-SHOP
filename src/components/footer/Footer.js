4b343f9d3aed7ac9ac20010e967110c0bf7ed521
Kingsley O. Ifeanyi <80105176+Techillionaire@users.noreply.github.com>
2022-07-13 15:14:16 +0100

    .. (#62)

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,49 +12,7 @@ const Footer = () => {
     <footer className="bd-footer align-middle mt-5">
       <div className="container py-5 ">
         <div className="row">
-          {/* <div className="col-lg-3 mb-3 col-sm-12"> */}
-          {/* <Link to={"/"} className="flex-start">
-              <img src={Logo} alt="fabgarage brand" className="logo" />
-            </Link>
-            <p className="footerDesc mt-3">
-              Welcome to Nigeria’s First Online Fabrication Service Provider.
-            </p> */}
-
-          {/* <div className="socialMedia">
-              <Link to={"/"}>
-                <img
-                  src={Facebook}
-                  alt="fabgarage brand"
-                  className="socialMediaIcons"
-                />
-              </Link>
-
-              <Link to={"/"}>
-                <img
-                  src={Instagram}
-                  alt="fabgarage brand"
-                  className="socialMediaIcons"
-                />
-              </Link>
-
-              <Link to={"/"}>
-                <img
-                  src={LinkedIn}
-                  alt="fabgarage brand"
-                  className="socialMediaIcons"
-                />
-              </Link>
-
-              <Link to={"/"}>
-                <img
-                  src={Whatsapp}
-                  alt="fabgarage brand"
-                  className="socialMediaIcons"
-                />
-              </Link>
-            </div> */}
-          {/* </div> */}
-          <div className="col-lg offset-lg-1 mb-3 links col-sm-12">
+          <div className="offset-lg-1 mb-3 links  col-md col-4">
             <h5 className="footerLinkHeader mb-3">Products</h5>
             <ul className="list-unstyled">
               <li className="mb-2">
@@ -101,7 +59,10 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="col-lg mb-3">
+          
+          
+          
+          <div className="mb-3 col-md col-4">
             <h5 className="footerLinkHeader mb-3">Sercives</h5>
             <ul className="list-unstyled">
               <li className="mb-2">
@@ -116,7 +77,7 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="col-lg mb-3">
+          <div className=" mb-3 col-md col-4">
             <h5 className="footerLinkHeader mb-3">Quick links</h5>
             <ul className="list-unstyled">
               <li className="mb-2">
@@ -131,7 +92,9 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="col-lg mb-3">
+
+
+          <div className="col-md mb-3 col-4">
             <h5 className="footerLinkHeader mb-3">Support</h5>
             <ul className="list-unstyled">
               <li className="mb-2">
@@ -146,7 +109,8 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="col-lg mb-3">
+
+          <div className=" col-md mb-3 col-4">
             <h5 className="footerLinkHeader mb-3">Contact us</h5>
             <ul className="list-unstyled">
               <li className="mb-2">
